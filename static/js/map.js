36ce856744a3ec29befaee88625ffe9448ada2e4
user <user@localhost>
2016-07-30 20:22:45 +0300

    Spawnpoint timer

--- static/js/map.js
+++ static/js/map.js
@@ -521,13 +521,22 @@ function pokestopLabel(lured, last_modified, active_pokemon_id, latitude, longit
 }
 
 function spawnLabel(spawnpoint_id, disappear_time, latitude, longitude) {
+	
+	var expire_date = new Date(current_date.getTime() + disappear_time);
+    var expire_time = expire_date.getTime();
+	
+	
   var str;
     str = `
       <div>
-        <b>Spawn point</b>
+        <b>Spawn point <b>${spawnpoint_id}</b></b>
       </div>
       <div>
         Location: ${latitude.toFixed(6)}, ${longitude.toFixed(7)}
+      </div>
+	  <div>
+        Spawns at ${pad(expire_date.getHours())}:${pad(expire_date.getMinutes())}:${pad(expire_date.getSeconds())}
+        <span class='label-countdown' disappears-at='${expire_time}'>(00m00s)</span>
       </div>
       <div>
         <a href='https://www.google.com/maps/dir/Current+Location/${latitude},${longitude}' target='_blank' title='View in Maps'>Get directions</a>
