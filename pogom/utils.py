3d966b4e97b3b9648c84c580a8d2d50629b89e7c
user <user@localhost>
2016-07-30 14:59:09 +0300

    Updated delays

--- pogom/utils.py
+++ pogom/utils.py
@@ -40,16 +40,16 @@ def get_args():
     parser.add_argument('-l', '--location', type=parse_unicode,
                         help='Location, can be an address or coordinates')
     parser.add_argument('-st', '--step-limit', help='Steps', type=int,
-                        default=12)
+                        default=7)
     parser.add_argument('-sd', '--scan-delay',
                         help='Time delay between requests in scan threads',
-                        type=float, default=5)
+                        type=float, default=4)
     parser.add_argument('-td', '--thread-delay',
                         help='Time delay between each scan thread loop',
-                        type=float, default=5)
+                        type=float, default=4)
     parser.add_argument('-ld', '--login-delay',
                         help='Time delay between each login attempt',
-                        type=float, default=5)
+                        type=float, default=4)
     parser.add_argument('-dc', '--display-in-console',
                         help='Display Found Pokemon in Console',
                         action='store_true', default=False)
